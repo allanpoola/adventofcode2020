5a8a4bbcfafcefedcec4fa044659ea40cc3e8dbe
allan <allan.poola@ria.ee>
2020-12-06 13:05:25 +0200

    day6.2

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ const start = () => {
 
     const inputArray = fs.readFileSync('./src/input.txt').toString().trimEnd().split(/\n\n/gm)
         .map((value) => value.split(/\n/g))
-        .map((value) => [value
-            .join('')
-            .split('')
-            .sort()
-            .filter((value, i, array) => value !== array[i+1])
-            .join('')
-        ])
-        .map((value) => value[0].length)
-        .reduce((prev, current) => prev + current)
-
-
+        .map((value) => ({gr_size: value.length, str: value
+                .join('')
+                .split('')
+                .sort()
+        }))
+        .map((main_value) => ({...main_value, str: main_value.str
+                .filter((value, i, array) => array
+                    .filter((sub_value) => sub_value == value)?.length == main_value.gr_size)
+                .filter((value, i, array) => value != array[i+1])
+        }))
+        .reduce((prev, current) => prev + current.str.length, 0);
 
     console.log(inputArray)
 
