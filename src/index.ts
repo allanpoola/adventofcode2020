7f8a30416a263bd88bbc6862d15a214aa826c8db
allan <allan.poola@ria.ee>
2020-12-06 11:57:39 +0200

    day6.1

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,21 @@ import fs from 'fs';
 
 const start = () => {
 
-    const rows: number[] = [];
-    rows.length = 128;
-    for (let i = 0; i < rows.length; i++) {
-        rows[i] = i;
-    }
-    const cols: number[] = [];
-    cols.length = 8;
-    for (let i = 0; i < cols.length; i++) {
-        cols[i] = i;
-    }
-
-    const keepHalf = (input: number[], part: string): number[] => {
-        const len = input.length;
-        if (part === 'R' || part === 'B') {
-            return input.slice(len/2);
-        }
-        return input.slice(0, (len/2));
-    }
-
-    const inputArray = fs.readFileSync('./src/input.txt').toString().split(/\n/gm)
-        .filter((value) => !!value)
-        .map((value) => [value.substring(0, 7), value.substring(7)]);
-
-    const result = inputArray.map((value) => {
-        const rowValue = value[0].split('').reduce((keepHalf), rows)[0];
-        const colValue = value[1].split('').reduce((keepHalf), cols)[0];
-        return rowValue * 8 + colValue;
-    })
+    const inputArray = fs.readFileSync('./src/input.txt').toString().trimEnd().split(/\n\n/gm)
+        .map((value) => value.split(/\n/g))
+        .map((value) => [value
+            .join('')
+            .split('')
+            .sort()
+            .filter((value, i, array) => value !== array[i+1])
+            .join('')
+        ])
+        .map((value) => value[0].length)
+        .reduce((prev, current) => prev + current)
 
 
-    const result2 = result
-        .sort()
-        .filter((value, index, array) =>
-            (array[index + 1] - value) == 2)
 
-    console.log(result2[0] + 1)
+    console.log(inputArray)
 
 
 };
