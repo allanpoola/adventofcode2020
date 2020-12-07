3b4892685cb824562e5f2d8bd5c7ce534d04fabd
allan <allan.poola@ria.ee>
2020-12-07 12:21:40 +0200

    day7.2

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,38 @@ import fs from 'fs';
 
 const start = () => {
 
+    const normalize = (str: string): string[] => {
+        const count = Number.parseInt(str.slice(0, str.indexOf(' ')));
+        if (isNaN(count)) {
+            return []
+        }
+        const ret: string[]  = []
+        ret.length = count
+        ret.fill(str.slice(str.indexOf(' ') + 1), 0, count)
+        return ret
+    }
 
-    const input2Array = fs.readFileSync('./src/input.txt').toString().trimEnd().split(/\n/gm)
+    const inputArray = fs.readFileSync('./src/input.txt').toString().trimEnd().split(/\n/gm)
         .map((value) => value.split('contain'))
+        .map((value) => [value[0].slice(0, value[0].lastIndexOf(' b')).trim(),
+            value[1].split(',').map((value) => value.slice(0, value.lastIndexOf(' b')).trim())])
 
-    console.log(input2Array)
-    let tmp2Array = ['shiny gold']
-    let resArray: string[] = []
-
+    let tmpArray = [
+        'shiny gold'
+    ]
+    let count = 0
     do {
-        tmp2Array = [...input2Array
-            .filter((value) => !!tmp2Array.find((value2) => value[1].includes(value2)))
-            .map((value) => value[0].trimEnd().slice(0, value[0].lastIndexOf(' b')))]
-        resArray = [...resArray, ...tmp2Array]
-    } while (tmp2Array.length)
-    console.log(resArray.sort().filter((value, i, array) => value.localeCompare(array[i+1])).length)
+        tmpArray = tmpArray.map((value) => inputArray
+            .filter((inputValue) => inputValue[0].includes(value))
+            .map((value) => (value[1] as string[]).map((value) => normalize(value)))
+        )
+            .reduce((prev, current) => [...prev, ...current])
+            .reduce((prev, current) => [...prev, ...current])
+            .reduce((prev, current) => [...prev, ...current])
+        count = count + tmpArray.length
+    } while (!!tmpArray.length)
+
+    console.log(count)
 };
 
 start();
